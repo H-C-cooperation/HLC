67f04c54b0263639f3c14f9054a0157f8e317320
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-05-21 15:58:57 +0900

    feat: 영화 페이지 구성 및 구현

---
+++ Frontend/HLC/src/components/movie/MainCarousel.vue
@@ -0,0 +1,141 @@
+<!-- <template>
+  <div class="mx-auto video">
+    <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
+      <Slide v-for="(movie) in movies" :key="movie.id">
+        <div>
+          <iframe 
+            :src="movie.youtube_url + '?autoplay=1&mute=1&controls=0'" 
+            width="1080px"
+            height="500px"
+            frameborder="0"
+            class="mb-3 center-block"
+          >
+          </iframe>
+          <h2>TOP {{ movies.indexOf(movie) + 1 }}</h2>
+          <p>{{ movie.title }}</p>
+        </div>
+  
+      </Slide>
+  
+      <template #addons>
+        <navigation>
+          <template #next>
+            <img src="@/components/icons/arrow_forward.png" alt="arrow" width="30px" height="30px">
+          </template>
+          <template #prev>
+            <img src="@/components/icons/arrow_back.png" alt="arrow" width="30px" height="30px">
+          </template>
+        </navigation>
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<script setup>
+import { useMovieStore } from '@/stores/movie';
+import { ref } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
+const currentSlide = ref(0)
+
+const slideTo = (val) => {
+  currentSlide.value = val
+}
+
+const store = useMovieStore()
+const movies = store.movies.slice(0, 10)
+
+</script>
+
+<style scoped>
+.video {
+  width: 80%;
+}
+</style> -->
+
+<template>
+  <div class="mx-auto video">
+    <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
+      <Slide v-for="(movie) in movies" :key="movie.id">
+        <div style="width: 80%;">
+          <div class="iframe-container">
+            <iframe 
+              :src="movie.youtube_url + '?autoplay=1&mute=1&controls=0'" 
+              width="100%"
+              height="100%"
+              frameborder="0"
+              class="iframe-content"
+            ></iframe>
+          </div>
+          <div class="text-container">
+            <h2>TOP {{ movies.indexOf(movie) + 1 }}</h2>
+            <p>{{ movie.title }}</p>
+          </div>
+        </div>
+      </Slide>
+      <template #addons>
+        <navigation>
+          <template #next>
+            <img src="@/components/icons/arrow_forward.png" alt="arrow" width="30px" height="30px">
+          </template>
+          <template #prev>
+            <img src="@/components/icons/arrow_back.png" alt="arrow" width="30px" height="30px">
+          </template>
+        </navigation>
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<script setup>
+import { useMovieStore } from '@/stores/movie';
+import { ref } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
+const currentSlide = ref(0)
+
+const slideTo = (val) => {
+  currentSlide.value = val
+}
+
+const store = useMovieStore()
+const movies = store.movies.slice(0, 10)
+
+</script>
+
+<style scoped>
+.video {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.iframe-container {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 56.25%; /* 16:9 비율의 비디오를 위한 값 */
+}
+
+.iframe-content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.text-container {
+  margin-top: 10px;
+  text-align: center; 
+}
+
+h2 {
+  margin: 5px 0;
+}
+
+p {
+  margin: 0;
+}
+</style>
---
+++ Frontend/HLC/src/components/movie/SubCarousel5.vue
@@ -0,0 +1,125 @@
+<template>
+  <Carousel 
+    :itemsToShow="items" 
+    :wrapAround="true" 
+    :transition="500"
+    navigationNextLabel=""
+    navigationPrevLabel=""
+  >
+    <Slide v-for="movie in movies" :key="movie.id">
+      <img class="carousel__item" :src="`https://image.tmdb.org/t/p/w200/${movie.poster_path}`" :alt="movie.title" />
+    </Slide>
+
+    <template #addons>
+      <navigation>
+        <template #next>
+          <img src="../icons/arrow_forward.png" alt="arrow" width="30px" height="30px">
+        </template>
+        <template #prev>
+          <img src="../icons/arrow_back.png" alt="arrow" width="30px" height="30px">
+        </template>
+      </navigation>
+    </template>
+  </Carousel>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import { useMovieStore } from '@/stores/movie'
+import axios from 'axios'
+import 'vue3-carousel/dist/carousel.css'
+
+const store = useMovieStore()
+const movies = ref([])
+const items = ref(0)
+
+const getMovies = function() {
+  axios({
+  method: 'get',
+  url: `${store.API_URL}/api/v1/movies/`,
+  headers: {
+    Authorization: `Token ${store.token}`
+  },
+  params: {
+    mode: 'genre',
+    inputGenre: '범죄'
+  }
+})
+  .then(res => {
+    movies.value = res.data
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+const updateItems = () => {
+  if (window.innerWidth >= 1400) {
+    items.value = 8
+  } else if (window.innerWidth >= 1200) {
+    items.value = 7
+  } else if (window.innerWidth >= 992) {
+    items.value = 6
+  } else if (window.innerWidth >= 768) {
+    items.value = 5
+  } else if (window.innerWidth >= 576) {
+    items.value = 4
+  } else {
+    items.value = 3
+  }
+}
+
+onMounted(() => {
+  getMovies()
+  updateItems()
+  window.addEventListener('resize', updateItems)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateItems)
+})
+</script>
+
+<style scoped>
+
+.carousel__slide {
+  padding: 5px;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
+}
+</style>
