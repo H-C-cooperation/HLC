81bc6e9ec3b10456123caae07104d461a7a58d05
Ji Won Choi <jiwonchoi3794@gmail.com>
2024-05-21 20:14:58 +0900

    fix:actor 모델 : popularity

--- Backend/movies/serializers.py
+++ Backend/movies/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Actor, Movie, Review, Genre
+from accounts.serializers import UserProfileSerializer
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,12 +8,19 @@ class MovieSerializer(serializers.ModelSerializer):
         # fields = ('title', 'overview',)
         fields = '__all__'
         
-# 리뷰 전체 조회할 때 만 사용
+# 단일 영화 리뷰 전체 조회할 때 사용
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer()
+    formatted_updated_at = serializers.SerializerMethodField()
+
     class Meta:
         model = Review
-        fields = ('id', 'content', 'rate', 'updated_at',)
-        read_only_fields = ('movie', 'user',)
+        fields = ('id', 'content', 'rate', 'updated_at','formatted_updated_at', 'user', 'like_users')
+        read_only_fields = ('movie', 'user', 'like_users')
+
+    def get_formatted_updated_at(self, obj):
+        # 원하는 포맷으로 날짜를 변환합니다.
+        return obj.updated_at.strftime("%Y-%m-%d %H:%M:%S")
         
 class ReviewDetailSerializer(serializers.ModelSerializer):
     class MovieTitleSerializer(serializers.ModelSerializer):
@@ -27,11 +35,16 @@ class ReviewDetailSerializer(serializers.ModelSerializer):
         read_only_fields = ('movie', 'user', 'like_users',)
         
 class MovieDetailSerializer(serializers.ModelSerializer):
-    class ActorNameSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Actor
-            fields = ('id', 'name', )
-    actors = ActorNameSerializer(many=True ,read_only=True)
+    
+    actors = serializers.SerializerMethodField()
+    def get_actors(self, instance):
+        class ActorNameSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = Actor
+                fields = ('id', 'name', 'popularity',)
+                
+        serializer = ActorNameSerializer(instance.actors.order_by('-popularity'), many=True, read_only=True)
+        return serializer.data
     
     class GenreNameSerializer(serializers.ModelSerializer):
         class Meta:
