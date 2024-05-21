c6b1fb3d4a0ebcec78360650c6ed7226a8e7e8e8
ji won choi <jiwonchoi3794@gmail.com>
2024-05-21 10:37:16 +0900

    fix:User모델 image field 추가

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -7,4 +7,5 @@ from django.contrib.auth.models import AbstractUser
 # 유저 커스텀한 유저를 사용하기 위한 작업1
 class User(AbstractUser):
     # nickname = models.CharField(max_length=100)
-    followers = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='followings')
+    image = models.ImageField(upload_to='images/', default='images/sample.png')
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical = False ,related_name='followings')
